d2c73c3cbe611b5555c75a2284ec8f2f8346c0e5
kelleighm <kelleighmmartin@gmail.com>
2024-07-05 15:09:49 -0500

    added api

---
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/TravelPlannerApplication.java
@@ -0,0 +1,13 @@
+package Java_and_The_Scripts.travel_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlannerApplication.class, args);
+	}
+
+}
