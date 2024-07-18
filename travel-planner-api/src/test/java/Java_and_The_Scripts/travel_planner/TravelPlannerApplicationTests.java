8fd551ae24d6558af9900f5cbf05b175fe064d3e
kelleighm <154389573+kelleighm@users.noreply.github.com>
2024-07-17 19:44:04 -0500

    Setup (#2)
    * added .gitignore
    
    * added vite front end template
    
    * added api

---
+++ travel-planner-api/src/test/java/Java_and_The_Scripts/travel_planner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_and_The_Scripts.travel_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
