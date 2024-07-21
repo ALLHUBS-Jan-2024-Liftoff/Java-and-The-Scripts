65b173bcca95aba74528eb11dafba9c5953f921a
kelleighm <kelleighmmartin@gmail.com>
2024-07-20 21:52:32 -0500

    Initial TravelPlan

--- travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/models/TravelPlan.java
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/models/TravelPlan.java
@@ -3,21 +3,41 @@ package Java_and_The_Scripts.travel_planner.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class TravelPlan {
     @Id
     @GeneratedValue
     private Long id;
+
     private Long userId;
+
+    @NotBlank(message = "Destination is required")
     private String destination;
+
     private String startDate;
+
     private String endDate;
+
     private String description;
 
-
+    // NO ARGUMENT CONSTRUCTOR
     public TravelPlan() {
     }
+
+    // CONSTRUCTOR
+    public TravelPlan(Long id, Long userId, String destination, String startDate, String endDate, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.destination = destination;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    //GETTERS AND SETTERS
+
 
     public Long getId() {
         return id;
