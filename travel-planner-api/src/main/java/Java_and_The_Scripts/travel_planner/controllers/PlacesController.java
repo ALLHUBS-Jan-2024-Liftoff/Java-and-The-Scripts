35c36341cf36c53ac16742e4430ae0d77a3e2313
Your Name"kamalaGIT <bandhavi.kk@gmail.com>
2024-07-25 10:47:51 -0500

    PlacesController is added

--- travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/controllers/PlacesController.java
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/controllers/PlacesController.java
@@ -1,6 +1,9 @@
 package Java_and_The_Scripts.travel_planner.controllers;
 
+import Services.PlacesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- travel-planner-api/src/main/java/Services/PlacesService.java
+++ travel-planner-api/src/main/java/Services/PlacesService.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PlacesService {
 
+    public String getPlaces(String location) {
+
+    }
 }
