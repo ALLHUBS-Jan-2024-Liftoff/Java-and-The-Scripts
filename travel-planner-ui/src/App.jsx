007696776eea2f37158cd94d66f6f81f4e10ac0c
kelleighm <kelleighmmartin@gmail.com>
2024-07-28 21:44:58 -0500

    added navigation bar

--- travel-planner-ui/src/App.jsx
+++ travel-planner-ui/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <h1 class="text-center">Travel Planner App</h1>
+    <Header />
   )
 }
 
---
+++ travel-planner-ui/src/Components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/"><strong>Travel Planner</strong></Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/travel-plans" className="nav-link">Travel Plans</Nav.Link>
+            <Nav.Link as={Link} to="/create-travel-plan" className="nav-link">Create Travel Plan</Nav.Link>
+            <Nav.Link as={Link} to="/reviews" className="nav-link">Reviews</Nav.Link>
+            <Nav.Link as={Link} to="/create-review" className="nav-link">Create Review</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Header;
