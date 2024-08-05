c9228cf9ddf1e981c39c6c0a0c1582152d9230cd
kelleighm <kelleighmmartin@gmail.com>
2024-08-04 20:45:42 -0500

    Updates

--- travel-planner-ui/src/App.jsx
+++ travel-planner-ui/src/App.jsx
@@ -6,7 +6,9 @@ import Home from './Components/Home/Home';
 import Profile from './Components/Profile/Profile'; 
 import TravelPlanForm from "./Components/TravelPlan/TravelPlanForm"; 
 import TravelPlansList from './Components/TravelPlansList/TravelPlansList';
+import EditTravelPlan from './Components/EditTravelPlan/EditTravelPlan';
 import CreateReview from './Components/CreateReview/CreateReview';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} /> 
           <Route path="/create-travel-plan" element={<TravelPlanForm />} />
+          <Route path="/edit-travel-plan/:id" element={<EditTravelPlan />} />
           <Route path="/travel-plans" element={<TravelPlansList />} />
           <Route path="/create-review" element={<CreateReview />} />
         </Routes>
--- travel-planner-ui/src/Components/TravelPlansList/TravelPlansList.jsx
+++ travel-planner-ui/src/Components/TravelPlansList/TravelPlansList.jsx
@@ -2,26 +2,37 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios'; 
 import { Link } from 'react-router-dom';
+import { dataForTravelPlans } from '../../TestingData/TestingData';
 
 const TravelPlansList = () => {
     const [travelPlans, setTravelPlans] = useState([]); 
 
 
     useEffect(() => {
-        axios.get('/api/travelplans')
-            .then(response => {
+        // axios.get('/api/travelplans')
+        //     .then(response => {
 
-                setTravelPlans(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching travel plans:', error);
-            });
+        //         setTravelPlans(response.data);
+        //     })
+        //     .catch(error => {
+        //         console.error('Error fetching travel plans:', error);
+        //     });
+
+        setTravelPlans(dataForTravelPlans);
 }, []); 
 
 return (
     <div>
         <h1>All Travel Plans</h1>
-            <p>No travel plans available.</p>
+        <ol>
+            {travelPlans.map(plan => (
+                <li key={plan.id}>
+                    <Link to={`/travel-plans/${plan.id}`}>{plan.destination}</Link>
+                    <button onClick={() => handleEdit(plan.id)}>Edit</button>
+                    <button onClick={() => handleDelete(plan.id)}>Delete</button>
+                </li>
+            ))}
+        </ol>
     </div>
 );
 
