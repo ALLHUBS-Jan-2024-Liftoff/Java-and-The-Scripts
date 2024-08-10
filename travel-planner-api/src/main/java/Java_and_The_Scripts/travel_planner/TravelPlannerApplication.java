cb70fc6511a2db27031d9d0945a33881c7c5c2fb
kelleighm <kelleighmmartin@gmail.com>
2024-08-10 16:21:42 -0500

    Updates

--- travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/TravelPlannerApplication.java
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/TravelPlannerApplication.java
@@ -2,9 +2,7 @@ package Java_and_The_Scripts.travel_planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class TravelPlannerApplication {
@@ -14,18 +12,3 @@ public class TravelPlannerApplication {
 	}
 
 	}
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/api/**")
-//						.allowedOrigins("http://localhost:5173")
-//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//						.allowedHeaders("*")
-//						.allowCredentials(true);
-//			}
-//		};
-//	}
-//}
