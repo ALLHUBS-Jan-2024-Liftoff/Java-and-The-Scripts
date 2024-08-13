b354207e672d4595f710d3dc2d15bb1e57f35327
esteff2000 <steffensoner@gmail.com>
2024-08-12 20:29:37 -0500

    activity controller and repository

--- travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/repositories/ActivityRepository.java
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/repositories/ActivityRepository.java
@@ -2,8 +2,14 @@ package Java_and_The_Scripts.travel_planner.repositories;
 
 import Java_and_The_Scripts.travel_planner.entities.ActivityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<ActivityEntity,Long> {
+    @Query(value = "FROM ActivityEntity activity WHERE activityEntity.activityID = :activityIdPassed")
+    List<ActivityEntity> findByActivityId(@Param("activityIdPassed") Long activityId);
 }
