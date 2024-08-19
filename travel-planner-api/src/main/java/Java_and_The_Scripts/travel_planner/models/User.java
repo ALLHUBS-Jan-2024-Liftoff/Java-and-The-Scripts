ae90176a2a7a94d9e9aeeab4cdb3a28a5a1b954f
Bwizz245 <brwilliamsstl@gmail.com>
2024-08-19 17:33:47 -0500

    Setup API handling of login requests

--- travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/models/User.java
+++ travel-planner-api/src/main/java/Java_and_The_Scripts/travel_planner/models/User.java
@@ -2,8 +2,10 @@ package Java_and_The_Scripts.travel_planner.models;
 
 public class User {
     private Long id;
+    private String email;
     private String firstName;
     private String lastName;
+    private String password;
 
     public User() {
     }
@@ -22,6 +24,14 @@ public class User {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -36,5 +46,13 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
